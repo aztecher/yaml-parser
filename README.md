7aa98bd2103618d92e3549f51e15c9553449f4f2
aztecher <mikiyaf.business@gmail.com>
2020-02-07 23:06:03 +0900

    [add] create some command and lose to yaml

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # parsec-test
+
+敗北した  
+ここまでが限界  
+
+```shell
+ghci> pp (top <* eof) sampleText
+
+[ Block
+    [ Mapping ( Key "name" )
+        ( Literal ( Scalar "play-ground" ) )
+    ]
+, Block
+    [ Mapping ( Key "version" )
+        ( Literal ( Scalar "0.1.0." ) )
+    ]
+, Block
+    [ Mapping ( Key "github" )
+        ( Literal ( Scalar "githubuser/play-ground" ) )
+    ]
+, Block
+    [ Mapping ( Key "license" )
+        ( Literal ( Scalar "BSD" ) )
+    ]
+, Block
+    [ Mapping ( Key "author" )
+        ( Literal ( Scalar "Author name here" ) )
+    ]
+, Block
+    [ Mapping ( Key "maintainer" )
+        ( Literal ( Scalar "example@example.com" ) )
+    ]
+, Block
+    [ Mapping ( Key "copyright" )
+        ( Literal ( Scalar "2019 Author name here" ) )
+    ]
+, Block
+    [ Mapping ( Key "extra-source-files" )
+        ( Block
+            [ Sequence
+                [ Literal ( Scalar "README.md" )
+                , Literal ( Scalar "ChangeLog.md" )
+                ]
+            ]
+        )
+    ]
+, Block
+    [ Mapping ( Key "library" )
+        ( Block
+            [ Mapping ( Key "source-dirs" )
+                ( Literal ( Scalar "src" ) )
+            ]
+        )
+    ]
+]
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
+{-# LANGUAGE LambdaCase        #-}
 module MegaparsecSample.Sample where
 import           Control.Applicative        hiding (many)
 import           Control.Monad
-import           Data.Char                  (isSeparator, isSpace)
+import           Data.Char                  (isSeparator, isSpace, isAlpha)
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import           Data.Void
@@ -14,28 +15,38 @@ import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import           Text.Megaparsec.Debug      (dbg)
 import           Text.Pretty.Simple
+
 newtype Key = Key Text deriving (Eq, Show)
 newtype Scalar = Scalar Text deriving (Eq, Show)
+
 data Sentence where
   Sequence  :: [Yaml] -> Sentence
   Mapping   :: Key -> Yaml -> Sentence
     deriving (Eq, Show)
+
 data Yaml where
   Literal :: Scalar -> Yaml
   Block   :: [Sentence] -> Yaml
     deriving (Eq, Show)
+
 type Parser = Parsec Void Text
+
 lineComment :: Parser ()
 lineComment = L.skipLineComment "#"
+
 scn :: Parser ()
 scn = L.space space1 lineComment empty
+
 sc :: Parser ()
 sc = L.space (void $ some (char ' ' <|> char '\t')) lineComment empty
+
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
+
 ------------------------------------------------------------------------------------------------
 isLiteral :: Char -> Bool
 isLiteral c = c `notElem` [' ', '\n']
+
 litStr :: Parser Text
 litStr = lexeme $ try quoted <|> takeWhile1P (Just "litStr") isLiteral
   where
@@ -44,15 +55,67 @@ litStr = lexeme $ try quoted <|> takeWhile1P (Just "litStr") isLiteral
       str <- takeWhileP (Just "quoted") (/= '"')
       void (char '"')
       pure str
+
  -- TODO: fix: '-'から始まるkeyが取得できてしまう
 pKey :: Parser Key
-pKey = dbg "pKey" $ lexeme $ Key <$> takeWhile1P (Just "Key") (`notElem` (":" :: String))
+pKey = dbg "pKey" $ lexeme $ Key <$> key'
+  where
+    key' = T.cons <$> (satisfy isAlpha) <*> (takeWhile1P (Just "Key") (`notElem` (":" :: String)))
+
 pYaml :: Parser Yaml
 pYaml =  lexeme $ pLiteral <|> pBlock
+
+-- pYaml :: Parser Yaml
+-- pYaml = try noPattern <|> try somePattern <|>  try manyPattern
+--           where
+--             singleton :: Key -> Yaml -> Yaml
+--             singleton key = (Block . (\x -> [x]) . Mapping key)
+--             noPattern = do
+--               key <- pKey
+--               pColon
+--               dbg "noPattern" $ singleton key <$> lexeme pLiteral
+--             somePattern = dbg "somePattern" $ L.indentBlock scn $ do
+--               key <- pKey
+--               pColon
+--               return (L.IndentSome Nothing (pure . singleton key . Block) pMapping)
+--             manyPattern = dbg "manyPattern" $ L.indentBlock scn $ do
+--               key <- pKey
+--               pColon
+--               return (L.IndentMany Nothing (pure . singleton key . Block . (\x -> [x]) . Sequence) pOneItem)
+
 pBlock :: Parser Yaml
 pBlock = Block <$> many (dbg "Mapping" pMapping)
-top :: Parser Yaml
-top = Block . (\x -> [x]) . Sequence <$> many (L.nonIndented scn (dbg "allPattern" p))
+
+
+-- top :: Parser Yaml
+-- top = go
+--   where
+--     go = L.nonIndented scn (dbg "allPattern" p) >>= \case
+--       l@Literal _ -> pure l
+--       Block
+--         where
+--           singleton :: Key -> Yaml -> Yaml
+--           singleton key = (Block . (\x -> [x]) . Mapping key)
+--           p :: Parser Yaml
+--           p = try noPattern <|> try somePattern <|>  try manyPattern
+--               where
+--                 noPattern = do
+--                   key <- pKey
+--                   pColon
+--                   dbg "noPattern" $ singleton key <$> lexeme pLiteral
+--                 somePattern = dbg "somePattern" $ L.indentBlock scn $ do
+--                   key <- pKey
+--                   pColon
+--                   return (L.IndentSome Nothing (pure . singleton key . Block) pMapping)
+--                 manyPattern = dbg "manyPattern" $ L.indentBlock scn $ do
+--                   key <- pKey
+--                   pColon
+--                   return (L.IndentMany Nothing (pure . singleton key . Block . (\x -> [x]) . Sequence) pOneItem)
+
+
+top :: Parser [Yaml]
+top = many (L.nonIndented scn (dbg "allPattern" p))
+-- top = Block . (\x -> [x]) . Sequence <$> many (L.nonIndented scn (dbg "allPattern" p))
   where
     singleton :: Key -> Yaml -> Yaml
     singleton key = (Block . (\x -> [x]) . Mapping key)
@@ -71,49 +134,63 @@ top = Block . (\x -> [x]) . Sequence <$> many (L.nonIndented scn (dbg "allPatter
             key <- pKey
             pColon
             return (L.IndentMany Nothing (pure . singleton key . Block . (\x -> [x]) . Sequence) pOneItem)
+
 pColon :: Parser ()
 pColon = lexeme $ void (char ':')
+
 pHyphen :: Parser ()
 pHyphen = lexeme $ void (char '-')
+
 pMapping :: Parser Sentence
 pMapping = dbg "pMapping" $ do
   key <- pKey
   pColon
   yaml <- pYaml
   return $ Mapping key yaml
+
 pOneItem :: Parser Yaml
 pOneItem = dbg "pOneItem" $ pHyphen *> pYaml
+
 pScalar :: Parser Scalar
 pScalar = Scalar <$> litStr
+
 pLiteral :: Parser Yaml
 pLiteral = Literal <$> pScalar
+
 pSentence :: Parser Sentence
 pSentence = pMapping
+
 main :: IO ()
 main = do
   withFile "package.yaml" ReadMode $ \handle -> do
     str <- hGetContents handle
     putStr . show $ str
+
 pMapping' = L.nonIndented scn (L.indentBlock scn p)
   where
     p = do
       header <- pKey
       pColon
       return (L.IndentMany Nothing (return . (header, )) pMapping)
+
 pMapping'' = L.nonIndented scn (dbg "somePattern" $ L.indentBlock scn p)
   where
     p = do
       root <- pKey
       pColon
       return (L.IndentSome Nothing (pure . Block) pMapping)
+
 pItemList = L.nonIndented scn (L.indentBlock scn p)
   where
     p = do
       header <- pItem
       return (L.IndentMany Nothing (return . (header, )) pItem)
+
 pItem = lexeme (some (alphaNumChar <|> char '-')) <?> "list item"
+
 pp :: Show a => Parser a -> Text -> IO ()
 pp parser src = either print pPrint $ parse parser "for pretty print" src
+
 sampleText :: Text
 sampleText = T.init $ T.unlines ["name:                play-ground"
                                 , "version:             0.1.0."
@@ -127,4 +204,9 @@ sampleText = T.init $ T.unlines ["name:                play-ground"
                                 , "  - ChangeLog.md"
                                 , "library:"
                                 , "  source-dirs: src"
+                                -- , "  test:"
+                                -- , "    testkey1:"
+                                -- , "      testkey2: value"
+                                -- , "    testkey3:"
+                                -- , "      testkey4: value"
                                 ]
